353da2a299b07d971c081ab24a5f7d7635307a0c
Shannon Kuah <shankuah62@gmail.com>
2019-11-11 18:48:34 +1300

    listing fix maybe?

--- js/ptfinder.js
+++ js/ptfinder.js
@@ -19,39 +19,41 @@ function loadMemberjsonText()
         {
             console.log(xhr.responseText);
             const jsonText = JSON.parse(xhr.responseText);
-            let tablejsonText = "";
+            let fcMemberText = "";
+            let partyMemberText = "";
 
-            jsonText.array.forEach(jsonText => 
+            jsonText.forEach(data => 
             {
                 let fcMemberTable = "<tr>";
-                fcMemberTable += "<td>" + jsonText.Player + "</td>";
-                fcMemberTable += jsonText.PLD != 0 ? "<td>" + jsonText.PLD + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.WAR != 0 ? "<td>" + jsonText.WAR + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.DNK != 0 ? "<td>" + jsonText.DNK + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.GNB != 0 ? "<td>" + jsonText.GNB + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.WHM != 0 ? "<td>" + jsonText.WHM + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.SCH != 0 ? "<td>" + jsonText.SCH + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.AST != 0 ? "<td>" + jsonText.AST + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.MNK != 0 ? "<td>" + jsonText.MNK + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.DRG != 0 ? "<td>" + jsonText.DRG + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.NIN != 0 ? "<td>" + jsonText.NIN + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.SAM != 0 ? "<td>" + jsonText.SAM + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.BRD != 0 ? "<td>" + jsonText.BRD + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.MCH != 0 ? "<td>" + jsonText.MCH + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.DNC != 0 ? "<td>" + jsonText.DNC + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.BLM != 0 ? "<td>" + jsonText.BLM + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.SMN != 0 ? "<td>" + jsonText.SMN + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                fcMemberTable += jsonText.RDM != 0 ? "<td>" + jsonText.RDM + "</td>" : "<i class=\"tiny material-icons\">block</i>"
-                tablejsonText += fcMemberTable;
+                fcMemberTable += "<td>" + data.Player + "</td>";
+                fcMemberTable += data.PLD != 0 ? "<td>" + data.PLD + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.WAR != 0 ? "<td>" + data.WAR + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.DNK != 0 ? "<td>" + data.DNK + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.GNB != 0 ? "<td>" + data.GNB + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.WHM != 0 ? "<td>" + data.WHM + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.SCH != 0 ? "<td>" + data.SCH + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.AST != 0 ? "<td>" + data.AST + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.MNK != 0 ? "<td>" + data.MNK + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.DRG != 0 ? "<td>" + data.DRG + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.NIN != 0 ? "<td>" + data.NIN + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.SAM != 0 ? "<td>" + data.SAM + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.BRD != 0 ? "<td>" + data.BRD + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.MCH != 0 ? "<td>" + data.MCH + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.DNC != 0 ? "<td>" + data.DNC + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.BLM != 0 ? "<td>" + data.BLM + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.SMN != 0 ? "<td>" + data.SMN + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberTable += data.RDM != 0 ? "<td>" + data.RDM + "</td>" : "<i class=\"tiny material-icons\">block</i>"
+                fcMemberText += fcMemberTable;
 
                 let partyMemberTable = "";
-                partyMemberTable += "<td>" + jsonText.Player + "</td><td><div class=\"switch\"><label>Off<input type=\"checkbox\"><span class=\"lever\"></span>On</label></div";
-                
-                if (!fcMembers.includes(jsonText.Player))
-                    fcMembers.push(jsonText.Player);
+                partyMemberTable += "<td>" + data.Player + "</td><td><div class=\"switch\"><label>Off<input type=\"checkbox\"><span class=\"lever\"></span>On</label></div";
+                partyMemberText += partyMemberTable
+
+                if (!fcMembers.includes(data.Player))
+                    fcMembers.push(data.Player);
             });
-            memberTable.innerHTML = tablejsonText;
-            partyTable.innerHTML = partyMemberTable;
+            memberTable.innerHTML = fcMemberText;
+            partyTable.innerHTML = partyMemberText;
         }        
     }
 
@@ -63,7 +65,7 @@ let characterRoles = [];
 
 function getPartyMembers()
 {
-    
+
 }
 
 function checkRoles()
